7c7f475d720bb9fe62fab4925ecd8a21059917f6
JAIN-SWAS <jainswasti.2002@gmail.com>
2026-01-24 11:44:04 -0500

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DB_URL = os.getenv("EVENTFLOW_DB_URL", "sqlite:///./eventflow.db")
+DB_URL = os.getenv("DATABASE_URL", os.getenv("EVENTFLOW_DB_URL", "sqlite:///./eventflow.db"))
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
